45b2e87484d15ac23bce916beb4551a689e0111d
calcutter <calcutter>
2017-12-05 15:55:35 -0600

    day 3 - further attempts

--- day3.playground/Contents.swift
+++ day3.playground/Contents.swift
@@ -2,13 +2,21 @@
 //http://adventofcode.com/2017/day/3
 import Foundation
 
-func calcMoveDistance(_ step: Int) -> Int {
-    let root = sqrt(Double(step))
+func calcMoveDistance(_ value: Int) -> Int {
+    let root = sqrt(Double(value))
     let ceiling = Int(ceil(root))
-    let halfCeiling = ceiling / 2
-    let offset = step % 2 == 0 ? 0 : 1
+    let ring = ceiling / 2
     
-    return halfCeiling + offset
+    //temp hack
+    if ring == 0 {
+        return 0
+    }
+    
+    if ring % 2 == 1 {
+        return ring + value % (ring + 1)
+    } else {
+        return ring + (value - ring) % (ring + 1)
+    }
 }
 
 calcMoveDistance(1) == 0
@@ -20,9 +28,11 @@ calcMoveDistance(6) == 1
 calcMoveDistance(7) == 2
 calcMoveDistance(8) == 1
 calcMoveDistance(9) == 2
-calcMoveDistance(12) //== 3
-calcMoveDistance(23) //== 2
-calcMoveDistance(24) //== 3
-calcMoveDistance(25) //== 4
+calcMoveDistance(12) == 3
+calcMoveDistance(23) == 2
+calcMoveDistance(24) == 3
+calcMoveDistance(25) == 4
+calcMoveDistance(49) //== 6
+calcMoveDistance(50) //== 7
 calcMoveDistance(1024) //== 31
 calcMoveDistance(347991) // ?
